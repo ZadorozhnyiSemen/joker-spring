09bf0299a0ecbd4b8990ee802ddc011ab0008797
Zadorozhnyy Semen <zadorozhnyysemen@gmail.com>
2017-12-03 21:02:00 +0300

    joker-spring/ new proxy logic added

--- src/main/java/com/epam/spring/postprocessor/ProfilingHandlerBeanPostProcessor.java
+++ src/main/java/com/epam/spring/postprocessor/ProfilingHandlerBeanPostProcessor.java
@@ -1,18 +1,18 @@
 package com.epam.spring.postprocessor;
 
+import com.epam.spring.ProfilingController;
 import com.epam.spring.annotations.Profiling;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.lang.Nullable;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ProfilingHandlerBeanPostProcessor implements BeanPostProcessor {
     private Map<String, Class> map = new HashMap<>();
+    private ProfilingController controller = new ProfilingController();
 
     @Nullable
     @Override
@@ -29,10 +29,17 @@ public class ProfilingHandlerBeanPostProcessor implements BeanPostProcessor {
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
         Class beanClass = map.get(beanName);
         if (beanClass != null) {
-            return Proxy.newProxyInstance(beanClass.getClassLoader(), beanClass.getInterfaces(), new InvocationHandler() {
-                @Override
-                public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                    return null;
+            return Proxy.newProxyInstance(beanClass.getClassLoader(), beanClass.getInterfaces(), (proxy, method, args) -> {
+                if (controller.isEnabled()) {
+                    System.out.println("Profiling....");
+                    long before = System.nanoTime();
+                    Object invokeVal = method.invoke(bean, args);
+                    long after = System.nanoTime();
+                    System.out.println(after - before);
+                    System.out.println("End");
+                    return invokeVal;
+                } else {
+                    return method.invoke(bean, args);
                 }
             });
         }
