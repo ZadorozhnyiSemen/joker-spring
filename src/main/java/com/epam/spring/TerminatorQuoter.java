189adcede6d5a1774e73ef99f33568ab6a9847e4
Zadorozhnyy Semen <zadorozhnyysemen@gmail.com>
2017-12-03 20:35:17 +0300

    joker-spring/ post construct added

--- src/main/java/com/epam/spring/TerminatorQuoter.java
+++ src/main/java/com/epam/spring/TerminatorQuoter.java
@@ -2,6 +2,8 @@ package com.epam.spring;
 
 import com.epam.spring.annotations.InjectRandomInt;
 
+import javax.annotation.PostConstruct;
+
 public class TerminatorQuoter implements Quoter {
 
     @InjectRandomInt(min = 2, max = 7)
@@ -9,6 +11,7 @@ public class TerminatorQuoter implements Quoter {
 
     private String message;
 
+    @PostConstruct
     public void init() {
         System.out.println("Phase 2");
         System.out.println(repeat);
