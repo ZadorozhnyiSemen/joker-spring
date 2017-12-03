277d29261595b532ffaf6cb5f3f1dfb2bdfef00d
Zadorozhnyy Semen <zadorozhnyysemen@gmail.com>
2017-12-03 19:59:15 +0300

    joker-spring/ annotation added

--- src/main/java/com/epam/spring/TerminatorQuoter.java
+++ src/main/java/com/epam/spring/TerminatorQuoter.java
@@ -1,6 +1,11 @@
 package com.epam.spring;
 
+import com.epam.spring.annotations.InjectRandomInt;
+
 public class TerminatorQuoter implements Quoter {
+
+    @InjectRandomInt(min = 2, max = 7)
+    private int repeat;
 
     private String message;
 
@@ -14,6 +19,8 @@ public class TerminatorQuoter implements Quoter {
 
     @Override
     public void sayQuote() {
-        System.out.println("message = " + message);
+        for (int i = 0; i < repeat; i++) {
+            System.out.println("message = " + message);
+        }
     }
 }
---
+++ src/main/java/com/epam/spring/annotations/InjectRandomInt.java
@@ -0,0 +1,10 @@
+package com.epam.spring.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(value = RetentionPolicy.RUNTIME)
+public @interface InjectRandomInt {
+    int min();
+    int max();
+}
