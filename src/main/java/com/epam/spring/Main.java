d16e2d20259e29e8318db65d1041d0313796e886
Zadorozhnyy Semen <zadorozhnyysemen@gmail.com>
2017-12-03 22:30:49 +0300

    joker-spring/ small corrections

--- src/main/java/com/epam/spring/Main.java
+++ src/main/java/com/epam/spring/Main.java
@@ -5,6 +5,5 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
-        context.getBean(Quoter.class).sayQuote();
     }
 }
